8fe7c7301f779523bf830202d4a23f95e3cc3e16
Dallin Christensen <christensen.dallin@gmail.com>
2020-10-27 20:34:53 -0700

    reverting save uri return change

--- src/ImgMarkup/index.js
+++ src/ImgMarkup/index.js
@@ -208,7 +208,6 @@ const ImgMarkup = ({ children, imgSrc, imgStyles, onSave, defaultValues }) => {
   const save = async () => {
     const uri = await svgAsDataUri(document.querySelector('#svg-board'))
     onSave(uri)
-    return { uri }
   }
 
   const handleEditText = (e) => {
